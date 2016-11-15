13e559689db4b38ed5405e44be5db35637378157
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-11-14 22:52:29 -0500

    end of monday

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ const bodyParser = require('body-parser')
 
 app.use(cors());
 
-app.use( bodyParser.json() );  
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({     
-  extended: true
-})); 
-
-let users = [];
+app.use(bodyParser.urlencoded({
+     extended: true
+}));
 
 
-app.get('', function (req, res) {
+let users = [{ name: 'kristie', age: '18', id: '123', likesJS: true }, { name: 'Taco', age: '12', id: '12993', likesJS: true }];
+
+//RETURN aka GET users in array
+app.get('/user', function(req, res) {
+     res.json(users);
+     console.log(users)
 })
 
-
-app.post('', function (req, res) {
+//ADD aka POST new users
+app.post('/user', function(req, res) {
+     var newUser = req.body.newUser;
+     newUser = JSON.parse(newUser);
+     users.push(newUser); // ADD new user to array
+     console.log(newUser);
+     res.json(users); // RETURN array with new user ADDED
 })
 
-app.delete('', function(req,res) {
+//DELETE a user
+app.delete('/user', function(req, res) {
+
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, function() {
+     console.log('Example app listening on port 3000!')
 })
